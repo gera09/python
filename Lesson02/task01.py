d43572f19579506be7805fffbc08ca94fae6a633
gera09 <bp-sva@yandex.ru>
2019-09-02 19:05:04 +0300

    Lesson2 add new version

--- Lesson02/task01.py
+++ Lesson02/task01.py
@@ -1,11 +1,15 @@
 my_list_1 = [2, 5, 8, 2, 12, 12, 4]
 my_list_2 = [2, 7, 12, 3]
 
+for x in my_list_1[:]:
+    if x in my_list_2:
+        my_list_1.remove(x)
+"""
 for y in my_list_2:
     # print('y', y)
     if my_list_1.count(y) > 0:
         for x in range(my_list_1.count(y)):
             my_list_1.remove(y)
         # print(my_list_1)
-
+"""
 print(my_list_1)
